956b97842f5d071522b7d7e4dbf5e359c2b9ab23
moshenglei <moshenglei@zhongan.com>
2018-05-17 20:35:58 +0800

    init

--- src/main/java/com/panda/demo/SpringBootDemoApplication.java
+++ src/main/java/com/panda/demo/SpringBootDemoApplication.java
@@ -3,9 +3,11 @@ package com.panda.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.panda.demo.mapper")
+@ImportResource("classpath:beanRefContext.xml")
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/panda/demo/queryobject/RoomQuery.java
@@ -0,0 +1,6 @@
+package com.panda.demo.queryobject;
+
+import com.panda.demo.dataobject.RoomDO;
+
+public class RoomQuery extends RoomDO {
+}
