ce54992257d0b739380fd1c780aee34fc92ced25
mosl <moshenglei@icloud.com>
2018-05-20 03:05:06 +0800

    login

--- src/main/java/com/panda/demo/controller/ShopController.java
+++ src/main/java/com/panda/demo/controller/ShopController.java
@@ -4,6 +4,9 @@ import com.panda.demo.dto.ShopDto;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,5 +28,16 @@ public class ShopController {
             shopList.add(shop);
         }
         return shopList;
+    }
+
+
+
+    @RequestMapping(value = "/api/login", method = RequestMethod.POST)
+    public Object login(
+        @RequestParam String userName,
+        @RequestParam String password) {
+
+
+        return "moshenglei".equals(userName) && "19930205".equals(password);
     }
 }
